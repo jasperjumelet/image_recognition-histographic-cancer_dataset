470b23a2d4b2cd541680275c6ec7d10abad6dd4e
jasper jumelet <jasperjumelet@hotmail.com>
2019-12-14 20:19:23 +0100

    labels for input almost done

--- preprocessing_tensorflow.py
+++ preprocessing_tensorflow.py
@@ -1,7 +1,15 @@
 import tensorflow as tf
 from os import listdir
+import pandas as pd
 import numpy as np
 
 filenames = np.array([x for x in listdir('data/train')])
+df = pd.read_csv('data/train_labels.csv')
 
+# here we sort the labels and delete the filenames
+df = df.sort_values(by=['id'])
+df = df.drop(['id'], axis=1)
+labels = df.values
+print(labels)
+print(type(labels))
 print(filenames)
